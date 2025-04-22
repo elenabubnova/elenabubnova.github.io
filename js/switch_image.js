b2784661d3247913036b94d84f037c084ad08ff3
mintspace <dmitrybubnov.ca@gmail.com>
2025-04-22 00:40:04 -0700

    homepage video added

--- js/switch_image.js
+++
@@ -1,5 +0,0 @@
-function toggleImage(dark, light, imageSwitch, imageToggle) {
-    const image = document.getElementById(imageSwitch);
-    const toggle = document.getElementById(imageToggle);
-    image.src = toggle.checked ? dark : light;
-}
