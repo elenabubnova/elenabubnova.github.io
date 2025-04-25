d9ab823225f9d6876e9c9d9051f0e4e02975faca
mintspace <dmitrybubnov.ca@gmail.com>
2025-04-24 18:51:41 -0700

    helix gpt jpg updated

--- js/ui.js
+++ js/ui.js
@@ -9,16 +9,12 @@ if ($(".header").hasClass("header-black")) {
 }
 
 const $headerBlock = $(`
-			<a href="index.html" class="${link} logo">Elena<br>Bubnova</a>
+			<a href="index.html" class="${link} logo hover:text-fuchsia-600">Elena<br>Bubnova</a>
 			<div>
-				<a href="https://www.linkedin.com/in/elenabubnova/" target="_blank" class="${link} data-info">
+				<a href="https://www.linkedin.com/in/elenabubnova/" target="_blank" class="${link} data-info hover:text-fuchsia-600">
 					<i class="fa fa-linkedin fa-2x" aria-hidden="false"></i>
 				</a>
 			</div>
 		`);
 
 $(".header").append($headerBlock);
-
-
-// const vid = document.getElementById("myVideo");
-// vid.playbackRate = 2;
